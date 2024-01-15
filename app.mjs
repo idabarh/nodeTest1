78468fadbf5c0bbb4d5515cf95e0eed452071187
idabarh <idamba@uia.no>
2024-01-15 13:51:56 +0100

    first upload

---
+++ app.mjs
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+const log = console.log
+
+log(chalk.blue("Why"), chalk.red("do"), chalk.magenta("programmers"), chalk.blueBright("prefer"), chalk.green("dark"), chalk.redBright("mode?"));
+
+log(chalk.yellow("because"), chalk.black("lights"), chalk.cyan("attracts"), chalk.blue("bugs!"));
